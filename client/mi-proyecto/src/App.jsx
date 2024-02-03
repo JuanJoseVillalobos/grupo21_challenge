3341205f1484cd35ea6913d5bf0c3cefb8b716d3
unknown <aymeandrade2006@gmail.com>
2024-02-03 16:45:18 -0300

    :(

--- client/mi-proyecto/src/App.jsx
+++ client/mi-proyecto/src/App.jsx
@@ -8,19 +8,10 @@ import NavBar from './components/NavBar'
 function App() {
   return (
     <>
-      <head>
-        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Challenge</title>
-        <link rel="stylesheet" href="./src/assets/css/normalize.min.css" />
-        <link rel='stylesheet' href='./src/assets/css/font-awesome.min.css' />
-        <link rel="stylesheet" href="./src/assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css" />
-        <link rel="stylesheet" href="./src/assets/css/style.css" />
-      </head>
+      
       <div className="dashboard">
         <main className="content-wrap">
-          <Centro />
+          <Centro/>
         </main>
       </div>
     </>
