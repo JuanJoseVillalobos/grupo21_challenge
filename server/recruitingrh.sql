77d5172dfa54e7677c094fad573e4ecdee16fd57
Juan Villalobos <juanjosevillalobos.19@gmail.com>
2024-02-10 21:54:51 -0300

    Professions added to DB

--- server/recruitingrh.sql
+++ server/recruitingrh.sql
@@ -30,3 +30,16 @@ ADD CONSTRAINT `fk_professionId`
 FOREIGN KEY (`professionId`)
 REFERENCES `professions` (`id`);
 
+
+
+
+INSERT INTO `professions` (`id`, `name`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'abogado', '2024-02-11 00:49:36', '2024-02-11 00:49:36', NULL),
+(2, 'arquitecto', '2024-02-11 00:49:52', '2024-02-11 00:49:52', NULL),
+(3, 'botánico', '2024-02-11 00:50:30', '2024-02-11 00:50:30', NULL),
+(4, 'computista', '2024-02-11 00:50:44', '2024-02-11 00:50:44', NULL),
+(5, 'economista', '2024-02-11 00:50:54', '2024-02-11 00:50:54', NULL),
+(6, 'técnico de sonido', '2024-02-11 00:51:10', '2024-02-11 00:51:10', NULL),
+(7, 'profesor', '2024-02-11 00:51:17', '2024-02-11 00:51:17', NULL),
+(8, 'linguista', '2024-02-11 00:51:25', '2024-02-11 00:51:25', NULL);
+
