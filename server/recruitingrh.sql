55d5fdbbfa81dc4ccb496131bd7edee804f01f39
Juan Villalobos <juanjosevillalobos.19@gmail.com>
2024-02-19 18:46:06 -0300

    Student profession updated DB

--- server/recruitingrh.sql
+++ server/recruitingrh.sql
@@ -32,19 +32,19 @@ REFERENCES `professions` (`id`);
 
 
 
-
 INSERT INTO `professions` (`id`, `name`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'abogado', '2024-02-11 00:49:36', '2024-02-11 00:49:36', NULL),
-(2, 'arquitecto', '2024-02-11 00:49:52', '2024-02-11 00:49:52', NULL),
-(3, 'botánico', '2024-02-11 00:50:30', '2024-02-11 00:50:30', NULL),
-(4, 'computista', '2024-02-11 00:50:44', '2024-02-11 00:50:44', NULL),
-(5, 'economista', '2024-02-11 00:50:54', '2024-02-11 00:50:54', NULL),
-(6, 'técnico de sonido', '2024-02-11 00:51:10', '2024-02-11 00:51:10', NULL),
-(7, 'profesor', '2024-02-11 00:51:17', '2024-02-11 00:51:17', NULL),
-(8, 'linguista', '2024-02-11 00:51:25', '2024-02-11 00:51:25', NULL);
+(1, 'abogado', '2024-02-11 03:49:36', '2024-02-11 03:49:36', NULL),
+(2, 'arquitecto', '2024-02-11 03:49:52', '2024-02-11 03:49:52', NULL),
+(3, 'botánico', '2024-02-11 03:50:30', '2024-02-11 03:50:30', NULL),
+(4, 'computista', '2024-02-11 03:50:44', '2024-02-11 03:50:44', NULL),
+(5, 'economista', '2024-02-11 03:50:54', '2024-02-11 03:50:54', NULL),
+(6, 'técnico de sonido', '2024-02-11 03:51:10', '2024-02-11 03:51:10', NULL),
+(7, 'profesor', '2024-02-11 03:51:17', '2024-02-11 03:51:17', NULL),
+(8, 'linguista', '2024-02-11 03:51:25', '2024-02-11 03:51:25', NULL),
+(9, 'estudiante', '2024-02-19 21:41:59', '2024-02-19 21:41:59', NULL);
 
 INSERT INTO `candidates` (`id`, `dni`, `name`, `surname`, `email`, `phoneNumber`, `linkedin`, `birthdate`, `gender`, `image`, `professionId`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 46934967, 'Juan', 'Villalobos', 'juanjosevillalobos.19@gmail.com', '3884091630', 'https://www.linkedin.com/in/juan-jos%C3%A9-villalobos-b98741265/', '2006-03-19', 'male', NULL, 1, '2024-02-11 00:57:59', '2024-02-11 00:59:22', NULL),
-(2, 43698776, 'Francisco Javier', 'Paredes', 'franpa619@gmail.com', '3883330889', 'https://www.linkedin.com/in/francisco-paredes-354a2b26b/', '2001-10-15', 'male', NULL, 4, '2024-02-11 01:02:32', '2024-02-11 01:02:32', NULL),
-(3, 48016788, 'Giuliana Nahir', 'Bazzi', 'zulemanahir13@gmail.com', '3888573604', NULL, '2007-05-07', 'female', NULL, 7, '2024-02-11 01:03:49', '2024-02-11 01:03:49', NULL),
-(4, 47439466, 'Lucía Aymé', 'Andrade', 'aymeandrade2006@gmail.com', '3884707993', 'https://www.linkedin.com/in/luc%C3%ADa-aym%C3%A9-andrade-698b382b2/', '2006-09-19', 'female', NULL, 5, '2024-02-11 01:05:38', '2024-02-11 01:05:38', NULL);
+(1, 46934967, 'Juan', 'Villalobos', 'juanjosevillalobos.19@gmail.com', '3884091630', 'https://www.linkedin.com/in/juan-jos%C3%A9-villalobos-b98741265/', '2006-03-19', 'male', NULL, 9, '2024-02-11 03:57:59', '2024-02-19 21:42:18', NULL),
+(2, 43698776, 'Francisco Javier', 'Paredes', 'franpa619@gmail.com', '3883330889', 'https://www.linkedin.com/in/francisco-paredes-354a2b26b/', '2001-10-15', 'male', NULL, 9, '2024-02-11 04:02:32', '2024-02-19 21:42:25', NULL),
+(3, 48016788, 'Giuliana Nahir', 'Bazzi', 'zulemanahir13@gmail.com', '3888573604', NULL, '2007-05-07', 'female', NULL, 9, '2024-02-11 04:03:49', '2024-02-19 21:42:30', NULL),
+(4, 47439466, 'Lucía Aymé', 'Andrade', 'aymeandrade2006@gmail.com', '3884707993', 'https://www.linkedin.com/in/luc%C3%ADa-aym%C3%A9-andrade-698b382b2/', '2006-09-19', 'female', NULL, 9, '2024-02-11 04:05:38', '2024-02-19 21:42:35', NULL);
